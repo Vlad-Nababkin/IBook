dd758940396f6b28d8ad4ea1e8be6967727ea8a1
lsk-t <sashaswrc@yandex.ru>
2025-02-19 20:15:09 +0500

    feat/log_reg/

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { useState, useEffect } from 'react'
-// import MainPage from './pages/MainPage/MainPage';
-// import TasksPage from './pages/TasksPage/TasksPage';
-// import TestPage from './pages/TestPage/TestPage';
-// import Layout from './widgets/Layout/Layout';
-// import BooksPage from './pages/BooksPage/BooksPage';
 import LoginPage from './pages/LoginPage/LoginPage'
-// import OneBook from './pages/OneBookPage/OneBookPage';
+
 import RegPage from './pages/RegPage/RegPage';
-// import { setAccessToken } from './shared/lib/axiosinstance'
-// import UserApi from './entities/user/UserApi'
+
 
 
 function App() {
